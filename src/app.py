95852bad3627ffcdba1ad0b0effd4c4032c69057
Moshe Reubinoff <moshe.r@claroty.com>
2018-10-18 11:18:53 +0300

    adding client build

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "You are up and running"
+	return render_template('index.html')
+
