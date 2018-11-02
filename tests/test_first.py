0d9fdc9b417b32859394cf8e32557d6459e49367
Moshe Reubinoff <moshe.r@claroty.com>
2018-11-02 10:54:47 +0200

    adding test and codecov

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,15 +1,36 @@
 import pytest
-import os
 
-from src.data_models.db import DBSessionFactory
 from src.handlers.expenses import ExpenseHandler
-from src.data_models.models import	Base
+from .setting_env import db_factory
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
 
-def test_my_first_test():
-	db_factory = DBSessionFactory.setup(DATABASE_URL, Base)
+def test_add_get_Expense(db_factory):
 	handler = ExpenseHandler(db_factory)
-	a = handler.add_expense("moshe")
-	assert a is not None
-	assert True
+	expense = handler.add_expense("moshe")
+	assert expense is not None
+	new_expense = handler.get_expense_by_id(expense.id)
+	assert new_expense is not None
+
+def test_update_get_Expense(db_factory):
+	handler = ExpenseHandler(db_factory)
+	expense = handler.add_expense("moshe")
+	assert expense is not None
+	expense = handler.update_expense(expense.id, name="moshe_2")
+	new_expense = handler.get_expense_by_id(expense.id)
+	assert new_expense is not None
+	assert new_expense.name == "moshe_2"
+
+
+def test_delete_get_Expense(db_factory):
+	handler = ExpenseHandler(db_factory)
+	expense = handler.add_expense("moshe")
+	assert expense is not None
+	new_expense = handler.get_expense_by_id(expense.id)
+	assert new_expense is not None
+	new_expense = handler.delete_expense_by_id(expense.id)
+	try:
+		new_expense = handler.get_expense_by_id(expense.id)
+	except KeyError:
+		return
+	assert False
+
