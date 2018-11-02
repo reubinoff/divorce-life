6728c302ecfd337b11ca27248cc034c01b554fc7
Moshe Reubinoff <moshe.r@claroty.com>
2018-11-02 10:30:52 +0200

    adding test to db

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,11 +1,14 @@
 import pytest
 import os
 
-from .setting_env import db_factory
+from src.data_models.db import DBSessionFactory
 from src.handlers.expenses import ExpenseHandler
+from src.data_models.models import	Base
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
-def test_my_first_test(db_factory):
+def test_my_first_test():
+	db_factory = DBSessionFactory.setup(DATABASE_URL, Base)
 	handler = ExpenseHandler(db_factory)
 	a = handler.add_expense("moshe")
 	assert a is not None
